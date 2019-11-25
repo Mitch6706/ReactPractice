956628c6ba848060e42e47b2b13fe01503544ab2
Mitch6706 <51381515+Mitch6706@users.noreply.github.com>
2019-11-25 14:37:03 -0500

    commit
    commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,24 @@ import React from "react";
 import ZipForm from "./ZipForm";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zipcode: ""
+    };
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
   render() {
     return (
       <div className="app">
-        <ZipForm />
+        <ZipForm onSubmit={this.onFormSubmit} />
       </div>
     );
+  }
+
+  onFormSubmit(zipcode) {
+    this.setState({ zipcode });
   }
 }
 
